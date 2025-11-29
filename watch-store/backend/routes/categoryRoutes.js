f66641043d39b2b7d68aa6806e04400b5ba77688
hienasd112 <hiensao112@gmail.com>
2025-11-29 12:45:06 +0700

    1

--- watch-store/backend/routes/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { getCategories } from '../controllers/categoryController.js';
-
-router.route('/').get(getCategories);
-
-export default router;
